a4978d2bd896fb1c27418b2c4772fb862f704170
Simon Johansson <mail@simon-johansson.com>
2015-05-26 23:51:25 +0200

    Fix to PATH variable
    Now uses coreutils from brew and not vanilla OSX versions

--- zsh/exports.sh
+++ zsh/exports.sh
@@ -1,3 +1,3 @@
 
-export PATH=$PATH/usr/local/bin:/usr/local/opt/ruby/bin:/usr/local/mongo/bin:/usr/bin:/bin:/usr/sbin:/sbin
+export PATH=$(brew --prefix coreutils)/libexec/gnubin:/usr/local/bin:/usr/local/opt/ruby/bin:/usr/local/mongo/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH
 export EDITOR='subl'
