0806e2e2818b7dd1bb389b873f22b4e0279f1621
Simon Johansson <mail@simon-johansson.com>
2015-05-30 23:35:10 +0200

    Changes to brew installer
    Remove the Brewfile in favor of having everyting in the .sh file

--- installers/brew.sh
+++ installers/brew.sh
@@ -1,37 +1,110 @@
 #!/bin/bash
 
-DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-command_exists () {
-    type "$1" &> /dev/null;
-}
-
-printf "\nInstalling/Updating Brew\n"
-
-if command_exists 'brew'; then
-    brew update
-    brew upgrade --all
-    brew cleanup
-    brew cask cleanup
-else
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    # to be able to use brewfiles
-    brew tap Homebrew/bundle
-
-    brew tap caskroom/cask
-    # access specific versions to be able to install sublime-text3
-    brew tap caskroom/versions
-    # to be able to install fonts via cask
-    brew tap caskroom/fonts
-
-    # Thoughtbots rcm dotfile management lib
-    brew tap thoughtbot/formulae
-
+if !type 'brew' &> /dev/null; then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-cd "$DIR"
+# to be able to use brewfiles
+#brew tap Homebrew/bundle
 
-brew bundle
-brew bundle cleanup
+brew tap caskroom/cask
+# access specific versions to be able to install sublime-text3 & google-chrome-canary
+brew tap caskroom/versions
+# to be able to install fonts via cask
+brew tap caskroom/fonts
 
-cd -
+# Thoughtbots rcm dotfile management lib
+brew tap thoughtbot/formulae
+
+brew install 'coreutils'
+brew install 'findutils'
+brew install 'rename'
+brew install 'bash'
+brew install 'zsh'
+brew install 'wget'
+brew install 'tree'
+brew install 'node'
+brew install 'git'
+brew install 'autojump'
+brew install 'mongodb'
+brew install 'python'
+brew install 'unrar'
+brew install 'mackup'
+brew install 'karn'
+brew install 'rcm'
+
+# Cask
+brew install caskroom/cask/brew-cask
+
+# Programming
+brew cask install 'iterm2'
+brew cask install 'transmit'
+brew cask install 'gitx'
+brew cask install 'sublime-text3'
+
+# Browsers
+brew cask install 'google-chrome'
+brew cask install 'google-chrome-canary'
+brew cask install 'firefox'
+
+# Status bar apps
+brew cask install 'istat-menus'
+brew cask install 'flux'
+brew cask install 'caffeine'
+brew cask install 'alfred'
+brew cask install '1password'
+
+# Social
+brew cask install 'skype'
+brew cask install 'slack'
+
+# Cloud apps
+brew cask install 'google-drive'
+brew cask install 'dropbox'
+brew cask install 'evernote'
+
+# Media
+brew cask install 'vlc'
+brew cask install 'spotify'
+
+# Other
+brew cask install 'the-unarchiver'
+brew cask install 'mou'
+brew cask install 'balsamiq-mockups'
+brew cask install 'sdformatter'
+brew cask install 'appcleaner'
+
+# Games
+brew cask install 'steam'
+
+# Quicklook extension
+brew cask install 'qlcolorcode'
+brew cask install 'qlstephen'
+brew cask install 'qlmarkdown'
+brew cask install 'quicklook-json'
+brew cask install 'qlprettypatch'
+brew cask install 'quicklook-csv'
+brew cask install 'betterzipql'
+brew cask install 'webpquicklook'
+brew cask install 'suspicious-package'
+
+# Fonts
+brew cask install 'font-fontawesome'
+brew cask install 'font-source-code-pro'
+brew cask install 'font-source-code-pro-for-powerline'
+brew cask install 'font-inconsolata'
+brew cask install 'font-inconsolata-for-powerline'
+brew cask install 'font-droid-sans-mono'
+brew cask install 'font-droid-sans-mono-for-powerline'
+
+brew update
+brew upgrade brew-cask
+brew cleanup
+brew cask cleanup
+
+# cd "$DIR"
+# brew bundle
+# brew bundle cleanup
+# cd -
